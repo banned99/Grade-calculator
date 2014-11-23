d62d8b80c82711b8bba60fd865c9291e9462dc6d
Patchara Lee <s7070076@kmitl.ac.th>
2014-11-23 22:54:07 +0700

    Grade-calculator V1
    Update start command

--- grade-cal.py.py
+++ grade-cal.py.py
@@ -16,26 +16,60 @@ class App:
         Contains all widget in application window
         """
         ##Label for text -> Number of subject
-        text_sub = Label(master,text="Number of Subject :")
+        text_sub = Label(master, text="Number of Subject :", width=20)
 
         ##Entry for num of subject
-        number = IntVar()
-        num_sub = Entry(master, width=5, textvariable=number)
+        self.number = IntVar()
+        num_sub = Entry(master, width=5, textvariable=self.number)
 
         ##Button start
-        starter = Button(master, text="Start")
+        starter = Button(master, text="Start", command=self.start)
 
-        ##Button reset
-        reset = Button(master, text="Reset")
-
-        ##Window's grid arranger
+        ##Window arrangement section
         text_sub.grid(row=0, column=0, sticky=E)
         num_sub.grid(row=0, column=1, padx=5, pady=5)
         starter.grid(row=1, column=0)
-        reset.grid(row=1, column=1)
-        
+
+        ##Data for other methods
+        self.weight = []
+        self.subject = []
+
+    def start(self):
+        """
+        Create the input slot of subject
+        """
+        ##Get data section: number of subject from __init__
+        num_of_sub = self.number.get()
+
+        ##Text label section
+        Label(text=" ", width=10).grid(row=0, column=2)
+        Label(text="Weight :", width=10).grid(row=0, column=3)
+        Label(text="Score :", width=10).grid(row=0, column=4)
+
+        ##Create data for creating subject weight and subject score entry slots
+        for i in xrange(num_of_sub):
+            if num_of_sub < 1 or num_of_sub > 20:
+                pass
+            else:
+                self.subject.append("Subject "+str(i+1))
+                self.weight.append("Weight"+str(i+1))
+                
+        ##Create subject weight and subject score entry slots
+        for j in xrange(len(self.weight)):
+            text = Label(text=(self.subject[j]+str(' :')), width=15)
+            self.weight[j] = DoubleVar()
+            self.subject[j] = IntVar()
+            entry_subject = Entry(width=5, textvariable=self.subject[j])
+            entry_weight = Entry(width=5, textvariable=self.weight[j])
+
+            ##All text and entry arrangement section
+            text.grid(row=j+1, column=2, sticky=E)
+            entry_weight.grid(row=j+1, column=3, padx=5, pady=5)
+            entry_subject.grid(row=j+1, column=4, padx=5, pady=5)
+            
+    
 root = Tk()
 app = App(root)
-root.geometry('300x100+50+50')
+root.geometry('500x650-50-100')
 root.mainloop()
 root.destroy()
